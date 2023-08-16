f2f0fd0715c3c8daacea25e1e770a89901169249
shipshapebear <shipshapebear@gmail.com>
2023-08-17 00:33:03 +0800

    fix webpack-dev-middleware loop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import * as React from "react"
 import { Link, type HeadFC, type PageProps } from "gatsby"
-//import { Button, buttonVariants } from "@/components/button"
-//import { cn } from "@/lib/utils"
+import { Button, buttonVariants } from "@/components/button"
+import { cn } from "@/lib/utils"
 
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main>  
-      
-      {/* button usage */}
-      {/* <Button>Test Button</Button>
-      <Link to="/home" className={cn(buttonVariants({ variant: "destructive" }))}>Test</Link> */}
-    </main> 
+    <main>
+      <Button variant="default">Test Button</Button>
+      <Link to="/home" className={cn(buttonVariants({ variant: "destructive" }))}>Test</Link>
+    </main>
   )
 }
 
